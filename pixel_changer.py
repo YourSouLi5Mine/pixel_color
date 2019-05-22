959d6ab852f0753c21d54ec1514e73bb727ff384
Jorge Hristho Precich Serna <precichysim@gmail.com>
2019-05-21 23:48:49 -0500

    improve color finder

--- pixel_changer.py
+++ pixel_changer.py
@@ -1,54 +1,42 @@
-from __future__ import print_function
 import argparse
 import cv2
 import numpy as np
 from statistics import mean
 
-def getAreas(contours):
-    areas = []
-    for line_points in contours:
-        area = cv2.contourArea(line_points)
-        areas.append(area)
-    return areas
+def color_range(pixel):
+    lower =  np.array([pixel[0] - 50, 50, 50])
+    upper =  np.array([pixel[0] + 50, 255, 255])
+    return [lower,upper]
 
-def drawContours(areas, contours, image):
-    for index, area in enumerate(areas, start=0):
-        if (area > mean(areas)):
-            cnt  = contours[index]
-            rect = cv2.minAreaRect(cnt)
-            box  = cv2.boxPoints(rect)
-            box  = np.int0(box)
-            cv2.drawContours(image,[box],0,(0,0,255),2)
+def process(pixel, hsv, image, color):
+    spectrum    = color_range(pixel) 
+    mask        = cv2.inRange(hsv, spectrum[0], spectrum[1])
+    restore     = cv2.bitwise_and(image,image,mask=mask)
+    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    paint_image(contours, image, color)
+    return [restore, mask]
+
+def paint_image(contours, image, color):
+    for contour in contours:
+        cv2.drawContours(image, contour, -1, color, 2)
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", required = True, help = "Route to image")
 args = vars(ap.parse_args())
 
-image = cv2.imread(args["i"])
+image         = cv2.imread(args["i"])
+blurred_image = cv2.GaussianBlur(image, (5, 5), 0)
+hsv           = cv2.cvtColor(blurred_image, cv2.COLOR_BGR2HSV)
 
-hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+red_pixel = (4, 116.025, 223.89)
+red       = process(red_pixel, hsv, image, (0, 255, 0))
+cv2.imshow("Red Restored", red[0])
+cv2.imshow("Red Mask"    , red[1])
 
-inverse_red_mask  = cv2.inRange(hsv_image, (0,50,20), (5,255,255))
-red_mask          = cv2.inRange(hsv_image, (175,50,20), (180,255,255))
-inverse_gray_mask = cv2.inRange(hsv_image, (0,0,0), (155,155,155))
-gray_mask         = cv2.inRange(hsv_image, (255,255,255), (100,100,100))
+gray_pixel = (105, 21.675, 211.905)
+gray       = process(gray_pixel, hsv, image, (255, 0, 0))
+cv2.imshow("Gray Restored", gray[0])
+cv2.imshow("Gray Mask"    , gray[1])
 
-eyes_mask    = cv2.bitwise_or(inverse_red_mask, red_mask)
-mouth_mask   = cv2.bitwise_or(inverse_gray_mask, gray_mask)
-eyes_croped  = cv2.bitwise_and(image, image, mask=eyes_mask)
-mouth_croped = cv2.bitwise_and(image, image, mask=mouth_mask)
-
-eyes_contours, hierarchy  = cv2.findContours(eyes_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-mouth_contours, hierarchy = cv2.findContours(mouth_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-eyes_areas = getAreas(eyes_contours)
-mouth_areas = getAreas(mouth_contours)
-drawContours(eyes_areas, eyes_contours, image)
-drawContours(mouth_areas, mouth_contours, image)
-  
-cv2.imshow("eyes_mask", eyes_mask)
-cv2.imshow("eyes_croped", eyes_croped)
-cv2.imshow("mouth_mask", mouth_mask)
-cv2.imshow("mouth_croped", mouth_croped)
-cv2.imshow("image", image)
+cv2.imshow("Original", image)
 cv2.waitKey(0)
